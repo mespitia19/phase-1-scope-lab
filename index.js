9d7d12db4ea7d3c4d2851394ff4202efec081977
MaximoEspitia <max19espitia@gmail.com>
2022-02-27 14:07:27 -0600

    commit tree b8ea9e682321364892d64449e85f7caff160c75a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase()
+}
+
+var bestCustomer 
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+var bestCustomer
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jimmy'
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'not jimmy'
+}
